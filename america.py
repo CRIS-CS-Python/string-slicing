fb64934d06835032f5d3df5b395f4c5c5b98fba3
Teacher Isaac <teacher@eduk8.us>
2024-10-10 08:41:19 +0700

    added string slicing excercise

---
+++ america.py
@@ -0,0 +1,29 @@
+'''
+<Your Name>, <Grade> <Course>
+Assignment: String Slicing
+This assignment will demonstrate how to manipulate and print strings.
+'''
+
+# DO NOT EDIT THIS LINE
+country = "the united states of america"
+
+# 1. print the country in *lower case*
+
+# 2. print country in *title case*
+
+# 3. print country in *upper case*
+
+# 4. print the length of the country string as:
+#    string length of country: <length>
+
+# 5. slice the string "america" from country, upper case the 'a',
+#    and store it in a variable named `short_name` with a value "America"
+
+# 6. concantentate a string value with `short_name` to
+#    produce the name of the continent where the country resides
+#    and print the result:
+#    <title case country> is located within <title case continent>
+
+# 7. print a statement how we refer to people from the country:
+#    People from <title case country> are called <title case of people in plural>
+
